a95def05e5a64c36e6ca3f9f6ad1fb143df4f452
kaconk <harutonouciha@gmail.com>
2023-05-24 21:58:43 +0700

    add telegram bot

--- client.py
+++ client.py
@@ -4,17 +4,34 @@ import os
 from apscheduler.schedulers.background import BackgroundScheduler
 from dotenv import load_dotenv
 from paho.mqtt import client as mqtt_client
+from telegram import __version__ as TG_VER
+try:
+    from telegram import __version_info__
+except ImportError:
+    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+
+if __version_info__ < (20, 0, 0, "alpha", 1):
+    raise RuntimeError(
+        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
+        f"{TG_VER} version of this example, "
+        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
+    )
+
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
 
 load_dotenv()
 
 broker = os.getenv('BROKER')
 port = int(os.getenv('PORT'))
-topic = 'smarthome/power'
-client_id = 'kwh01'
+topic = 'smarthome/power3'
+client_id = 'kwh03'
 username = os.getenv('USERNAME')
 password = os.getenv('PASSWORD')
 device = os.getenv('DEVICE')
 baudrate = os.getenv('BAUDRATE')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 starttime = time.time()
 scheduler = BackgroundScheduler()
@@ -48,18 +65,55 @@ def connect_mqtt():
 def write_read(_clnt):
    
     try:
-        arduino.write(bytes('1', 'utf-8'))
+        #arduino.write(bytes('1', 'utf-8'))
         time.sleep(0.05)
-        data = arduino.readline()
+        #data = arduino.readline()
         #y = json.loads(data)
-        _clnt.publish(topic+'/'+client_id,data)
+        #_clnt.publish(topic+'/'+client_id,data)
         #print(broker + str(y["voltage"]))
+        print("executed")
     except serial.SerialException as e:
         print("Koneksi serial arduino error :", str(e))
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}! Welcome to Kaconk Smart Home ",
+    )
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    print("pesan help")
+    await update.message.reply_text("Help!")
+
+def lighton(bot, update):
+  chat_id = update.message.chat_id
+  bot.send_message(chat_id, text="Light has been turned ON")
+
+def lightoff(bot, update):
+  chat_id = update.message.chat_id
+  bot.send_message(chat_id, text="Light has been turned OFF")
+
+def given_message(bot, update):
+  text = update.message.text.upper()
+  text = update.message.text
+  if text == 'Turn on the Light':
+    lighton(bot,update)
+  
+  elif text == 'Turn off the Light':
+    lightoff(bot,update)
 
 if __name__ == '__main__':
     client = connect_mqtt()
     client.loop_start()
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help",help_command))
+
+    application.run_polling()
+
     try:
         while True:
             time.sleep(1)
